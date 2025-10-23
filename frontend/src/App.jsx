e33fe1a276d108e17ee0f815f0c3a1aa8165e7b1
barelezra10-create <barelezra10@gmail.com>
2025-10-23 12:10:38 +0300

    Add src App.jsx file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ImageUploader from "../components/ImageUploader.jsx";
+
+function App() {
+  return (
+    <div>
+      <ImageUploader />
+    </div>
+  );
+}
+
+export default App;
